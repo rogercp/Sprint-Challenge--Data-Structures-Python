e70faccc4d179420d2d60e747c10810ee514e744
Roger Caballero <rogercp1234@yahoo.com>
2019-09-20 09:25:37 -0700

    got names running in under 1 hundreth of a sec

--- names/names.py
+++ names/names.py
@@ -11,10 +11,25 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+sortedNames = names_1 + names_2
+
+sortedNames.sort()
+
+
+for i in range(len(sortedNames)):
+	if sortedNames[i] == sortedNames[i-1] :
+		duplicates.append(sortedNames[i])
+
+
+# log(n) runtime
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
